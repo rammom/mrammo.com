354be37fa69cdf9db55f86aea28398755b953ca0
moose <mr.rammo@gmail.com>
2019-04-19 19:05:48 -0400

    added projects and badges

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
+import { Container, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText, Row, Col, Badge, Button } from 'reactstrap';
 import Projects from '../projects.json';
 import './ProjectPage.css';
 import Header from '../components/Header';
@@ -11,15 +11,32 @@ export class ProjectPage extends Component {
 	render_projects() {
 		let items = [];
 		projects.forEach(project => {
+			let badges = [];
+			for (let i = 0; i < project.badges.length; ++i) {
+				let badge = project.badges[i];
+				badges.push(
+					<Badge href="#" color={badge.color} style={{marginRight: "0.3em"}} key={i}>{badge.language}</Badge>
+				)
+			}
 			items.push(
-				<a href={project.github} style={{ textDecoration: "none" }} key={project.name}>
+				<a href={project.github} style={{ textDecoration: "none", marginBottom: "0.5em" }} key={project.name}>
 					<ListGroupItem className="my_list_group_item" key={project.name}>
 						{/** Make row and columns */}
-						<ListGroupItemHeading>{project.name} </ListGroupItemHeading>
-						<ListGroupItemText>
-							{project.desc}
-						</ListGroupItemText>
-						<span style={{fontSize: "0.8em"}}>{project.date}</span>
+						<Row>
+							<Col>
+								<ListGroupItemHeading>{project.name} </ListGroupItemHeading>
+								<ListGroupItemText>
+									{project.desc}
+								</ListGroupItemText>
+								{badges}
+								<br />
+								<span style={{ fontSize: "0.8em" }}>{project.date}</span><br /><br />
+								{(project.demo) ? <a href={project.demo}><Button color="info" size="sm">Demo</Button></a> : null}
+							</Col>
+							<Col xl="3">
+								<img src={project.img} alt={project.img} height="150em" style={{ float: "right" }} />
+							</Col>
+						</Row>
 					</ListGroupItem>
 				</a>
 			);
